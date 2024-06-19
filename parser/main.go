9bcbe352b4006f8377708c868026e723d7fe9747
Paul Dale <ldpercy@gmail.com>
2024-06-19 16:08:40 +1000

    clean out old junk from main

--- parser/main.go
+++ parser/main.go
@@ -7,47 +7,11 @@ import (
 )
 
 func main() {
-
-	fmt.Println("silver-data-parser-go")
-
-	//testFolder := "./test/file/"
-	//testFilename := "factorial.agl"
-	//outputFilename := fmt.Sprintf("%s.ast", inputFilename)
-	//testFilepath := fmt.Sprintf("%v%v", testFolder, testFilename)
-	//outputFilepath := fmt.Sprintf("%s.ast.json", testFilepath)
-
-	/*
-		// Parse file content
-		content := test.ReadFile(testFilepath)
-
-		abstractSyntaxTree := schema.AstNode{
-			NodeType:  "document",
-			Text:      content,
-			LineStart: 1,
-			LineEnd:   1,
-			CharStart: 1,
-			CharEnd:   1,
-		}
-
-		tmp := schema.SilverLexer["document"]
-
-		lexing.Lex(&abstractSyntaxTree, &tmp, "document")
-
-		// Write output
-
-		json, err := json.MarshalIndent(abstractSyntaxTree, "", "	")
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-
-		test.WriteFile(outputFilepath, string(json))
-	*/
-
-	// files := test.GetFiles("./test/simple/")
-	//test.GenerateOutput(files)
+	fmt.Println("Module: silver-data-parser-go")
 
 	if len(os.Args) > 1 {
 		task := os.Args[1]
+		fmt.Println("Task:", task)
 		switch task {
 		case "test":
 			test.TestDirectory("./test/")
@@ -57,6 +21,6 @@ func main() {
 			fmt.Println("No matching task")
 		}
 	} else {
-		fmt.Println("Try: test")
+		fmt.Println("Tasks: test, generate")
 	}
 }
