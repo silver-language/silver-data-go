a2e3584f7c82b56792063d8c2aff8538eb87fb6c
Paul Dale <ldpercy@gmail.com>
2024-06-19 17:57:25 +1000

    change over to use cmd line flags

--- parser/main.go
+++ parser/main.go
@@ -1,26 +1,35 @@
+/* silver-data-parser-go
+
+Examples:
+go run . -task=test
+*/
+
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"silver-data/parser-go/test"
 )
 
 func main() {
 	fmt.Println("Module: silver-data-parser-go")
 
-	if len(os.Args) > 1 {
-		task := os.Args[1]
-		fmt.Println("Task:", task)
-		switch task {
-		case "test":
-			test.TestDirectory("./test/")
-		case "generate":
-			test.GenerateDirectoryOutput("./test/")
-		default:
+	taskPtr := flag.String("task", "none", "The task for the parser to perform")
+	directoryPtr := flag.String("directory", "./test/", "Directory for tests")
+	flag.Parse()
+
+	task := *taskPtr // os.Args[1]
+	fmt.Println("Task:", task)
+	switch task {
+	case "test":
+		test.TestDirectory(*directoryPtr)
+	case "generate":
+		test.GenerateDirectoryOutput(*directoryPtr)
+	default:
+		{
 			fmt.Println("No matching task")
+			fmt.Println("Tasks: test, generate")
 		}
-	} else {
-		fmt.Println("Tasks: test, generate")
 	}
 }
